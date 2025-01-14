b7116ed2995cdf34214957cef63b54ccba0f5a7e
noufanelachola <noufansadiqelachola@gmail.com>
2025-01-14 12:18:33 +0530

    tailwind css

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -3,7 +3,7 @@ import AddToCart from './AddToCart'
 
 const ProductCard = () => {
   return (
-    <div>
+    <div className='p-10 bg-white text-black text-2xl hover:bg-gray-500 hover:text-red-700'>
         <AddToCart/>
     </div>
   )
